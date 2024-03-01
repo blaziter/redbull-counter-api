0473783e798cb7c6273317a6b6875c73badd0b92
blaziter <petrblaztran@gmail.com>
2024-03-01 17:58:49 +0100

    feat(app): add environment variables, regenerate prisma schema, use jwt, configure cors and allow preflight request

--- process-env.d.ts
+++ process-env.d.ts
@@ -3,8 +3,12 @@ namespace NodeJS {
         [key: string]: string | undefined;
         HOSTNAME: string;
         PORT: string;
+        CLIENT_URL: string;
+        JWT_SECRET: string;
         SLACK_CLIENT_ID: string;
         SLACK_CLIENT_SECRET: string;
+        SLACK_OAUTH_V2_ACCESS_URL: string;
+        SLACK_SIGNIN_URL: string;
         SLACK_WEBHOOK_URL: string;
         DATABASE_PASSWORD: string;
         DATABASE_URL: string;
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,35 @@ import { Elysia } from 'elysia';
 
 import { slackRouter } from '@routes/slackRouter';
 import { auth } from '@utils/providers';
-
-
-const app = new Elysia()
-    .use(cors())
+import jwt from '@elysiajs/jwt';
+const app = new Elysia({ prefix: '/api' })
+    /**
+     * * This middleware is used to handle the CORS preflight request
+     * * https://github.com/elysiajs/elysia-cors/issues/48
+     */
+    .onAfterHandle(({ request, set }) => {
+        const allowHeader = set.headers['Access-Control-Allow-Headers'];
+        if (allowHeader === '*') {
+            set.headers['Access-Control-Allow-Headers'] =
+                request.headers.get('Access-Control-Request-Headers') ?? '';
+        }
+    })
+    .use(
+        cors({
+            origin: process.env.CLIENT_URL,
+        })
+    )
+    .use(
+        jwt({
+            name: 'jwt',
+            secret: process.env.JWT_SECRET,
+        })
+    )
     .use(auth)
     .use(slackRouter)
     .listen(
         {
-            port: process.env.PORT,
+            port: process.env.PORT || 3001,
             hostname: process.env.HOSTNAME,
         },
         ({ hostname, port }) => {
